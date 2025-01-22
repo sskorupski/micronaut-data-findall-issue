3fbe94ed9c42b4c07ca3ed65f3864cfa3dfc83f9
Sébastien Skorupski <sebastien.skorupski@gmail.com>
2025-01-22 02:56:32 +0100

    first commit

---
+++ src/test/java/com/sskorupski/micronaut/OrderRepositoryIT.java
@@ -0,0 +1,26 @@
+package com.sskorupski.micronaut;
+
+import io.micronaut.data.model.Pageable;
+import io.micronaut.data.model.Sort;
+import io.micronaut.data.repository.jpa.criteria.PredicateSpecification;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+@MicronautTest(transactional = false)
+public class OrderRepositoryIT {
+
+    @Inject
+    OrderRepository orderRepository;
+
+   @Test
+    void findAll_withPageableSort_andSearch() {
+        // GIVEN 
+       Sort.Order.Direction sortDirection = Sort.Order.Direction.ASC;
+        Pageable pageable = Pageable.UNPAGED.order(new Sort.Order("embedded.someProp", sortDirection, false));
+        PredicateSpecification<Order> predicate = null;
+        // WHEN
+        orderRepository.findAll(predicate, pageable);
+       // THEN no exception is thrown
+    }
+}
